886e98a0123ffcb0f4062a48efe4d7257f736cd4
lastromanticx <giladbarkan@live.com>
2016-07-23 11:18:05 -0400

    error wrong number of arguments (given 2, expected 0) @ /Users/flatironschool/.rvm/gems/ruby-2.3.1/gems/pundit-1.1.0/lib/pundit/rspec.rb:18:in 'initialize'

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,17 @@
 class UserPolicy < ApplicationPolicy
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record.try(:user) == user
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
 end
