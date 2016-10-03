98308295e008ab7d60ee7af9779ea2ae31347a39
Natalisp <nathalipat@gmail.com>
2016-10-03 14:35:27 -0400

    Done.

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,6 @@
+class VisitorsController < ApplicationController
+
+def about
+  render 'pages/about'
+end
+end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,19 @@
 class UserPolicy < ApplicationPolicy
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record == user
+    # scope.where(:id => record.id).exists?
+  end
+
+  def update?
+    user.admin? || record == user
+  end
+
+  def destroy?
+    user.admin?
+  end
+
 end
