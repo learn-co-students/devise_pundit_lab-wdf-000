233bab4990e6c6244c428de95f41bb22372d1f92
fidelscodes <fidelscodes@gmail.com>
2016-10-03 09:05:39 -0400

    add enum roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
   has_many :notes
   has_many :viewers
   has_many :readable, through: :viewers, source: :note
+
+    enum role: [:user, :moderator, :admin]
 end
