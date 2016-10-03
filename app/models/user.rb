03d5d790a4c5d916e3ffac621f2b5b8543764aa4
fidelscodes <fidelscodes@gmail.com>
2016-10-03 12:01:26 -0400

    remove confirmable, was breaking app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
   has_many :notes
   has_many :viewers
   has_many :readable, through: :viewers, source: :note
 
-    enum role: [:user, :moderator, :admin]
+  enum role: [:user, :moderator, :admin]
 end
