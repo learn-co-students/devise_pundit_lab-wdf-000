ed6c7cca596235bce7281a2816198813259b25b6
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-09 22:24:51 -0400

    added note policy test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum role: [:normal, :moderator, :admin]
+  enum role: [:user, :moderator, :admin]
   has_many :notes
   has_many :viewers
   has_many :readable, through: :viewers, source: :note
---
+++ app/policies/post_policy.rb
@@ -0,0 +1,27 @@
+class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def create?
+    user.admin? || user.moderator? || user.user?
+  end
+
+  def update?
+    user.admin? || record.try(:user) == user
+  end
+
+  def index?
+    user.admin? || user.moderator?
+  end
+
+  def show?
+    user.admin? || user.moderator? || record.try(:user) == user
+  end
+
+  def destroy?
+    user.admin? || record.try(:user) == user
+  end
+end
