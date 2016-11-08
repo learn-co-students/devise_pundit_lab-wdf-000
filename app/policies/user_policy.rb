690c7d073b646981e5b6e0e6bb77e9959bbcfd5b
Esmery Corniel <esmerycornielle@gmail.com>
2016-11-08 16:56:32 -0500

    Done

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,18 @@
 class UserPolicy < ApplicationPolicy
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+   user.admin? || record == user
+  end
+
+  def update?
+   user.admin? || record == user
+  end
+
+  def destroy?
+   user.admin? && (record != user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160119074046) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "role"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
